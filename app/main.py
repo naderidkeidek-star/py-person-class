ec2f3ef0fff8927e056f7ec90bb46896c65008c8
Nader Idkeidek <nader.idkeidek@gmail.com>
2026-06-04 11:48:29 +0300

    Implement Person class and create_person_list function
    1st Try

--- app/main.py
+++ app/main.py
@@ -1,8 +1,34 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+        Person.people[name] = self
 
 
-def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+def create_person_list(people):
+    Person.people = {}
+
+    persons = []
+
+    # Create all Person objects
+    for person_data in people:
+        person = Person(
+            person_data["name"],
+            person_data["age"]
+        )
+        persons.append(person)
+
+    # Create wife/husband links
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+
+        if "wife" in person_data and person_data["wife"] is not None:
+            person.wife = Person.people[person_data["wife"]]
+
+        if "husband" in person_data and person_data["husband"] is not None:
+            person.husband = Person.people[person_data["husband"]]
+
+    return persons
